8bf6ca8990209176b7e84407cb8987ae16b96a89
Roberto Júnior <rbezerra18@hotmail.com>
2021-04-03 19:27:30 -0300

    Build final. Implementar função pickImageFromGallery.

--- app/src/main/java/jr/roberto/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/jr/roberto/fotosbootcamp/MainActivity.kt
@@ -1,6 +1,8 @@
 package jr.roberto.fotosbootcamp
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -24,6 +26,19 @@ class MainActivity : AppCompatActivity() {
             } else {
                 pickImageFromGallery()
             }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            image_view.setImageURI(data?.data)
         }
     }
 
