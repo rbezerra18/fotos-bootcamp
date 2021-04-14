bdc84fbc774adf84ac4465535db41b3d506bb305
Roberto Júnior <rbezerra18@hotmail.com>
2021-04-13 21:58:01 -0300

    Build final.

--- app/src/main/java/jr/roberto/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/jr/roberto/fotosbootcamp/MainActivity.kt
@@ -2,22 +2,27 @@ package jr.roberto.fotosbootcamp
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var image_uri : Uri? = null
+
     companion object {
         private const val PERMISSION_CODE_IMAGE_PICK = 1000
         private const val IMAGE_PICK_CODE = 1001
 
         private const val PERMISSION_CODE_CAMERA_CAPTURE = 2000
-        private const val CAMERA_CAPTURE_CODE = 2001
+        private const val OPEN_CAMERA_CODE = 2001
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,15 +63,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openCamera() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, CAMERA_CAPTURE_CODE)
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nova foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Imagem capturada pela câmera")
+
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, OPEN_CAMERA_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             image_view.setImageURI(data?.data)
+        }
+
+        if(resultCode == Activity.RESULT_OK && requestCode == OPEN_CAMERA_CODE) {
+            image_view.setImageURI(image_uri)
         }
     }
 
